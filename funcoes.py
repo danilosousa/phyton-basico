8ed292ee09382f71353499e7065e3afea1469974
danilosousa <danilosousa.developer@gmail.com>
2026-06-12 20:34:56 -0300

    fix: remove unnecessary whitespace and ensure proper formatting in funcoes.py

--- funcoes.py
+++ funcoes.py
@@ -2,7 +2,7 @@ def calcularDesconto(preco, porcentagem):
     return preco - (preco * porcentagem / 100)
 
 if __name__ == "__main__":
-    
+
     valor_final = calcularDesconto(1040, 20)
 
     print(f"O valor final é de R$ {valor_final:.2f} reais")
