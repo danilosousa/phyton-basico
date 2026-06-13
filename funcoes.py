11b10a517939116002ad28ffd9c79700f9cb6994
danilosousa <danilosousa.developer@gmail.com>
2026-06-12 21:23:10 -0300

    feat: add age verification and BMI calculation functions to funcoes.py; update main.py to utilize new functions

--- funcoes.py
+++ funcoes.py
@@ -6,3 +6,24 @@ if __name__ == "__main__":
     valor_final = calcularDesconto(1040, 20)
 
     print(f"O valor final é de R$ {valor_final:.2f} reais")
+
+
+def verificar_idade(idade):
+    if idade >= 18:
+        return "Maior de idade"
+    else:
+        return "Menor de idade"
+
+if __name__ == "__main__":
+    idade = int(input("Digite a sua idade: "))
+    print(verificar_idade(idade))
+
+
+def calcular_imc(peso, altura):
+    return peso / (altura ** 2)
+
+if __name__ == "__main__":
+    peso = float(input("Digite o seu peso: "))
+    altura = float(input("Digite a sua altura: "))
+    print(calcular_imc(peso, altura))
+    print(f"Seu IMC é de {calcular_imc(peso, altura):.1f}")
--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from funcoes import verificar_idade, calcularDesconto, calcular_imc
+
 def mensagem_futuro(nome: str, idade: int, anos: int = 5) -> str:
     return f"Olá, {nome} daqui {anos} anos você tera {idade + anos} anos de idade"
 
@@ -7,3 +9,9 @@ if __name__ == "__main__":
     idade = int(input("Sua idade: "))
 
     print(mensagem_futuro(nome, idade))
+
+
+    print(verificar_idade(idade))
+    print(calcularDesconto(100, 10))
+    # print(calcular_imc(63, 1.4))
+    print(f"Seu IMC é de {calcular_imc(63, 1.6):.1f}")
